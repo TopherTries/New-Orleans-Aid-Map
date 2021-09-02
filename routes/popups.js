ab0387f04796f91a88cf3f813bbb5e219cfa1c17
George Farro <george@scandy.co>
2021-09-02 08:09:40 -0500

    access to certain routes with/without auth

--- routes/popups.js
+++ routes/popups.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { ensureAuth } = require('../middleware/auth')
-const { getPopupLocation, addPopupLocation } =  require('../controllers/popups')
+const { getPopupLocation, addPopupLocation } = require('../controllers/popups')
 const router = express.Router()
 
 const Popup = require('../models/Popup')
@@ -8,58 +8,55 @@ const Popup = require('../models/Popup')
 // @desc Show add page
 // @route GET /popups/add
 router.get('/add', ensureAuth, (req, res) => {
-    res.render('popups/add')
+  res.render('popups/add')
 })
 
 // @desc Process add form
 // @route POST /popups
 router.post('/', ensureAuth, async (req, res) => {
-    try {
-      req.body.user = req.user.id
-      await Popup.create(req.body)
-      res.redirect('/dashboard') 
-    } catch (error) {
-        console.error(err)
-        res.render('error/500')
-    }
+  try {
+    req.body.user = req.user.id
+    await Popup.create(req.body)
+    res.redirect('/dashboard')
+  } catch (error) {
+    console.error(err)
+    res.render('error/500')
+  }
 })
 
 router.route('/api/v1/popups').get(getPopupLocation).post(addPopupLocation)
 
 // @desc    Show all popups
 // @route   GET /popups
-router.get('/', ensureAuth, async (req, res) => {
-    try {
-      const popups = await Popup.find({ status: 'public' })
-        .populate('user')
-        .sort({ createdAt: 'desc' })
-        .lean()
-  
-      res.render('popups/index', {
-        popups,
-      })
-    } catch (err) {
-      console.error(err)
-      res.render('error/500')
-    }
-  })
+router.get('/', async (req, res) => {
+  try {
+    const popups = await Popup.find({ status: 'public' })
+      .populate('user')
+      .sort({ createdAt: 'desc' })
+      .lean()
+
+    res.render('popups/index', {
+      popups,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
 
 // @desc Show single popup
 // @route GET /popups/:id
-router.get('/:id', ensureAuth, async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
-    let popup = await Popup.findById(req.params.id)
-    .populate('user')
-    .lean()
+    let popup = await Popup.findById(req.params.id).populate('user').lean()
 
-    if(!popup){
+    if (!popup) {
       return res.render('error/404')
     }
 
     res.render('popups/show', {
-      popup
+      popup,
     })
-
   } catch (error) {
     console.error(err)
     res.render('error/404')
@@ -71,13 +68,13 @@ router.get('/:id', ensureAuth, async (req, res) => {
 router.get('/edit/:id', ensureAuth, async (req, res) => {
   try {
     const popup = await Popup.findOne({
-      _id: req.params.id
+      _id: req.params.id,
     }).lean()
-  
+
     if (!popup) {
       return res.render('error/404')
     }
-  
+
     if (popup.user != req.user.id) {
       res.redirect('/popups')
     } else {
@@ -97,27 +94,27 @@ router.put('/:id', ensureAuth, async (req, res) => {
   try {
     let popup = await Popup.findById(req.params.id).lean()
 
-  if (!popup) {
-    return res.render('error/404')
-  }
-
-  if (popup.user != req.user.id) {
-    res.redirect('/popups')
-  } else {
-    popup = await Popup.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true
-    })
-
-    res.redirect('/dashboard')
+    if (!popup) {
+      return res.render('error/404')
     }
-    } catch (error) {
+
+    if (popup.user != req.user.id) {
+      res.redirect('/popups')
+    } else {
+      popup = await Popup.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      })
+
+      res.redirect('/dashboard')
+    }
+  } catch (error) {
     console.error(err)
     return res.render('error/500')
   }
 })
 
-  // @desc Delete popup
+// @desc Delete popup
 // @route DELETE /stories/:id
 router.delete('/:id', ensureAuth, async (req, res) => {
   try {
@@ -135,20 +132,18 @@ router.get('/user/:userId', ensureAuth, async (req, res) => {
   try {
     const popups = await Popup.find({
       user: req.params.userId,
-      status: 'public'
+      status: 'public',
     })
-    .populate('user')
-    .lean()
+      .populate('user')
+      .lean()
 
     res.render('popups/index', {
-      popups
+      popups,
     })
   } catch (err) {
     console.error(err)
     res.render('error/500')
   }
 })
-
-
 
 module.exports = router
